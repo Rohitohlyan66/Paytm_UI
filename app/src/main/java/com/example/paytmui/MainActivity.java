ea3130aad63fccc18a088fb6b3375d6b79a3a0f6
Rohitohlyan66 <ohlyanrohit66@gmail.com>
2020-04-26 22:14:46 +0530

    Initial commit

---
+++ app/src/main/java/com/example/paytmui/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.paytmui;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_item,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
+
